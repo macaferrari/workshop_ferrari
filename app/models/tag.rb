e083fdc291e974c922f40893445f0e140d665a89
webtech <webtech@webtech-debian.webtech>
2023-06-10 17:06:45 -0400

    tags y hashtags tables

---
+++ app/models/tag.rb
@@ -0,0 +1,4 @@
+class Tag < ApplicationRecord
+    belongs_to :tweet
+    belongs_to :hashtag
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_175541) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_10_210146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.bigint "tweet_id"
+    t.bigint "hashtags_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hashtags_id"], name: "index_tags_on_hashtags_id"
+    t.index ["tweet_id"], name: "index_tags_on_tweet_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "body", default: "", null: false
@@ -36,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_175541) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tags", "hashtags", column: "hashtags_id"
+  add_foreign_key "tags", "tweets"
   add_foreign_key "tweets", "users"
 end
